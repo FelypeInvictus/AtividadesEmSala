d43433efa70161489d41c8121f32e6a361f9dbf5
Felype Invictus <89306240+FelypeInvictus@users.noreply.github.com>
2022-03-24 14:34:10 -0300

    Melhorias e tratamento de erros do JOptionPane

--- Condicionais/Exercicio01.java
+++ Condicionais/Exercicio01.java
@@ -1,8 +1,7 @@
-
 import javax.swing.JOptionPane;
 
 public class Exercicio01 {
-	
+
 	/*
 	 * * Crie um programa que vai receber as notas que uma pessoa tirou nas duas
 	 * materias de sua prova. Serão dois parâmetros para receber. um para receber a
@@ -18,7 +17,7 @@ public class Exercicio01 {
 	static final double NOTA_MINIMA_DE_PORTUGUES = 60;
 	static final double NOTA_MINIMA_DE_MATEMATICA = 60;
 	static final double NOTA_MINIMA_TOTAL = 150;
-	
+
 	public static void main(String[] args) {
 
 		double notaDePortugues = perguntaNotaDePortugues();
@@ -26,41 +25,58 @@ public class Exercicio01 {
 		boolean obteveNotaMinimaPorMateria = validaNotaMinimaPorMateria(notaDePortugues, notaDeMatematica);
 		double somaTotalDasNotas = somaTotalDasNotas(notaDeMatematica, notaDePortugues);
 		boolean obteveMinimaTotal = validaMinimaTotal(somaTotalDasNotas);
-		exibeResultado(obteveMinimaTotal,obteveNotaMinimaPorMateria, somaTotalDasNotas);
+		exibeResultado(obteveMinimaTotal, obteveNotaMinimaPorMateria, somaTotalDasNotas);
 	}
-	
-	static double perguntaNotaDePortugues () {
+
+	static double perguntaNotaDePortugues() {
+		try {
+		return Double.parseDouble(JOptionPane.showInputDialog("CALCULE SUAS NOTAS DE PORTUGUÊS E MATEMATICA \n\n\nDIGITE SUA NOTA DE PORTUGUÊS:"));
+		} catch (NumberFormatException e) {
+			System.exit(0); //Caso a variavel não receber nenhum dado, o programa será encerrado
+		} catch (NullPointerException e) {
+			System.exit(0); //Caso o usuario cancelar, o programa será encerrado
+		}
+
 		return Double.parseDouble(JOptionPane.showInputDialog("CALCULE SUAS NOTAS DE PORTUGUÊS E MATEMATICA \n\n\nDIGITE SUA NOTA DE PORTUGU�S:"));
 	}
-	
+
 	static double perguntaNotadeDeMatematica() {
+		try {
 		return Double.parseDouble(JOptionPane.showInputDialog("DIGITE SUA NOTA DE MATEMATICA:"));
+		} catch (NumberFormatException e) {
+			System.exit(0); //Caso a variavel não receber nenhum dado, o programa será encerrado
+		} catch (NullPointerException e) {
+			System.exit(0); //Caso o usuario cancelar, o programa será encerrado
+		}
+
+		return Double.parseDouble(JOptionPane.showInputDialog("DIGITE SUA NOTA DE MATEMATICA:"));
+
 	}
-	
+
 	static boolean validaNotaMinimaPorMateria(double notaDePortugues, double notaDeMatematica) {
 		return notaDePortugues >= NOTA_MINIMA_DE_PORTUGUES && notaDeMatematica >= NOTA_MINIMA_DE_MATEMATICA;
 	}
-	
+
 	static double somaTotalDasNotas(double notaDePortugues, double notaDeMatematica) {
-		return notaDeMatematica +  notaDePortugues;
+		return notaDeMatematica + notaDePortugues;
 	}
-	
+
 	static boolean validaMinimaTotal(double somaTotal) {
 		return somaTotal >= NOTA_MINIMA_TOTAL;
 	}
-	
-	static void exibeResultado (boolean obteveMinimaTotal, boolean obteveNotaMinimaPorMateria, double somaTotal) {
-		if (obteveMinimaTotal && obteveNotaMinimaPorMateria) {	
+
+	static void exibeResultado(boolean obteveMinimaTotal, boolean obteveNotaMinimaPorMateria, double somaTotal) {
+		if (obteveMinimaTotal && obteveNotaMinimaPorMateria) {
 			exibePositivo(somaTotal);
 		} else {
 			exibeNegativo();
 		}
 	}
-	
+
 	static void exibePositivo(double somaTotal) {
-		JOptionPane.showMessageDialog(null,"PARABÉNS VOCÊ PONTUOU " + somaTotal +". FOI APROVADO!");
+		JOptionPane.showMessageDialog(null, "PARABÉNS VOCÊ PONTUOU " + somaTotal + ". FOI APROVADO!");
 	}
-	
+
 	static void exibeNegativo() {
 		JOptionPane.showMessageDialog(null, "INFELIZMENTE VOCÊ NÃO ATINGIU A NOTA MINIMA PARA SER APROVADO.");
 	}
